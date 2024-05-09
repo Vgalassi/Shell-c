5195f2c29cc44a29731eef5a9376049a367e2029
MATHEUS GONÇALVES ANITELLI <matheus.ga3@puccampinas.edu.br>
2024-05-09 11:28:25 -0300

    cd <caminho>

--- main.c
+++ main.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
+void exec_comando_cd(const char *caminho) {
+    if(caminho == NULL) {
+        printf("Uso: cd <caminho>\n");
+    } else {
+        if(chdir(caminho) == 0) {
+            printf("Mudou para o diretório %s\n", caminho);
+        } else {
+            perror("chdir");
+        }
+    }
+}
 
-
-int main(){
-
+int main() {
     char comando[100];
     printf("-- Bob shell --\n\n\n");
-    
-    
-    while(1){
-        fgets(comando,100,stdin);
+
+    while(1) {
+        printf("> ");
+        fgets(comando, 100, stdin);
         comando[strcspn(comando, "\n")] = 0;
-        if(strcmp(comando,"exit") == 0){
+
+        if(strcmp(comando, "exit") == 0) {
             printf("\nTerminou o BobShell\n");
             return 0;
         }
-        else{
-            printf("%s não é reconhecido como comando interno\n",comando);
+        else if(strcmp(comando, "cd") == 0) {
+            printf("Uso: cd <caminho>\n");
+        } 
+        else if(strncmp(comando, "cd ", 3) == 0) {
+            char *caminho = comando + 3;
+            exec_comando_cd(caminho);
+        } 
+        else {
+            printf("%s não é reconhecido como comando interno\n", comando);
         }
     }
+
+    return 0;
 }
