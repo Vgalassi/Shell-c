1ebb968f527b996d828b8a273942e642513d3080
Guto06 <agtomorato@gmail.com>
2024-05-16 11:36:37 -0300

    fix: fazendo o cat

--- main.c
+++ main.c
@@ -3,17 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 
-
-
 char** separarComando(char comando[],int *numeroEnderecos){
-
     int i = 0;
     *numeroEnderecos = 1;
     char** enderecosComando;
     while(comando[i] != '\0'){
         if(comando[i] == ' '){
             comando[i] = '\0';
-            
             while(comando[i+1] == ' ')
                 i++;
             if(comando[i+1] != '\0')
@@ -25,7 +21,6 @@ char** separarComando(char comando[],int *numeroEnderecos){
     int j = 0;
     enderecosComando = malloc ((*numeroEnderecos) * sizeof(char*));
     enderecosComando[0] = comando;
-    
     while(i<(*numeroEnderecos)){
         if(comando[j] == '\0'){
             while(comando[j+1] == ' ')
@@ -33,11 +28,8 @@ char** separarComando(char comando[],int *numeroEnderecos){
             enderecosComando[i] = &comando[j+1];
             i++;
         }
-
         j++;
-
     }
-
     printf("comando inteiro:");
     for(i=0;i<(*numeroEnderecos);i++){
         printf("%s ",enderecosComando[i]);
@@ -46,7 +38,6 @@ char** separarComando(char comando[],int *numeroEnderecos){
     return enderecosComando;
 }
 
-
 int contaPalavras(char** enderecosComando, int numeroEnderecos){
     int i = 0;
     int numeroPalavras = 0;
@@ -54,14 +45,10 @@ int contaPalavras(char** enderecosComando, int numeroEnderecos){
         numeroPalavras++;
         i++;
     }
-
     return numeroPalavras;
 }
 
-
-
 int main(){
-
     char comando[8191];
     char diretorio[256];
     char **path;
@@ -75,37 +62,24 @@ int main(){
     int pathTamanho = 0;
     path = malloc(1 * sizeof(char*));
     
-    
     while(1){
-
         printf("\n%s>",diretorio);
         fgets(comando,8191,stdin);
         comando[strcspn(comando, "\n")] = 0;
         enderecosComando = separarComando(comando,&numeroEnderecos);
-
-
         numeroPalavras = contaPalavras(&enderecosComando[enderecoAtual],numeroEnderecos);
-
         printf("\nEndereço atual: %d\n",enderecoAtual);
         printf("\nNumero de palavras: %d\n",numeroPalavras);
        
-        
         if(strcmp(enderecosComando[enderecoAtual],"cd") == 0) {  
-            
             enderecoAtual++;
-            
             if (chdir(enderecosComando[enderecoAtual]) == 0) {
                 getcwd(diretorio, sizeof(diretorio));
-                
             } 
             else {
                 perror("chdir");
-                
             }
-
-            
-        
-        }else if(strcmp(enderecosComando[enderecoAtual],"path") == 0){
+        } else if(strcmp(enderecosComando[enderecoAtual],"path") == 0){
             enderecoAtual++;
             if(numeroPalavras == (enderecoAtual)){
                 printf("PATH = ");
@@ -116,35 +90,44 @@ int main(){
             }else{
                 for(i = 0; i < pathTamanho; i++){
                     free(path[i]);
-                    
                 } 
-               
-               free(path);
+                free(path);
                 pathTamanho = 0;
                 path = malloc((numeroPalavras-1) * sizeof(char*));
-                
                 for(i = 0;i<(numeroPalavras-1);i++){
                     path[i] = malloc(256 * sizeof(char));
                     strncpy(path[i],enderecosComando[enderecoAtual + i],255);
-                    
                 }
                 pathTamanho = i;
-
             }
-   
-        }
-        else if(strcmp(enderecosComando[0],"exit") == 0){
+        } else if(strcmp(enderecosComando[enderecoAtual],"cat") == 0){
+            enderecoAtual++;
+            FILE *file;
+            int c;
+            if(enderecoAtual >= numeroEnderecos) {
+                printf("Por favor, forneça o nome do arquivo como argumento.\n");
+            } else {
+                file = fopen(enderecosComando[enderecoAtual], "r");
+                if(file == NULL) {
+                    printf("Não foi possível abrir o arquivo %s\n", enderecosComando[enderecoAtual]);
+                } else {
+                    while((c = fgetc(file)) != EOF) {
+                        putchar(c);
+                    }
+                    fclose(file);
+                }
+            }
+        } else if(strcmp(enderecosComando[0],"clear") == 0){
+            system("cls");
+        } else if(strcmp(enderecosComando[0],"exit") == 0){
             printf("\nTerminou o BobShell\n");
             return 0;
-        }else{
+        } else {
             printf("%s não é reconhecido como comando interno\n",enderecosComando[0]);
         }
         free(enderecosComando);
-
         numeroPalavras = 0;
         enderecoAtual = 0;
-        
     }
-
     free(path);
 }
