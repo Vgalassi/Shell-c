25dd14dbd8ea883941b801ed9372aed62ff76588
Matheus Anitelli <mattanitelli4@gmail.com>
2024-05-19 16:25:20 -0300

    arquivo <ls> concluído

--- ls.c
+++ ls.c
@@ -1,7 +1,26 @@
-DIR *dir;
-            struct dirent *ent;
-            if ((dir = opendir (diretorio)) != NULL) {
-                while ((ent = readdir (dir)) != NULL) {
-                    printf ("%s\n", ent->d_name);
-                }
-                closedir (dir);
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h> // Inclua este cabeçalho para usar opendir e readdir
+
+int main(int argc, char *argv[]) {
+    DIR *dir;
+    struct dirent *ent;
+
+    if (argc < 2) {
+        printf("Por favor, forneça o nome do diretório como argumento.\n");
+        return 1;
+    }
+
+    dir = opendir(argv[1]);
+    if (dir == NULL) {
+        printf("Não foi possível abrir o diretório %s\n", argv[1]);
+        return 2;
+    }
+
+    while ((ent = readdir(dir)) != NULL) {
+        printf("%s\n", ent->d_name);
+    }
+
+    closedir(dir);
+    return 0;
+}
