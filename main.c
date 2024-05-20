cc719b828965f7f9d0d71ca16791bc9c68bc0a3c
Vinicius Galassi <viniciusgalassi@gmail.com>
2024-05-20 19:30:17 -0300

    comando em paralelo

--- main.c
+++ main.c
@@ -34,10 +34,6 @@ char** separarComando(char comando[], int *numeroEnderecos) {
         }
         j++;
     }
-    printf("comando inteiro:");
-    for (i = 0; i < (*numeroEnderecos); i++) {
-        printf("%s ", enderecosComando[i]);
-    }
     printf("\n");
     return enderecosComando;
 }
@@ -45,10 +41,11 @@ char** separarComando(char comando[], int *numeroEnderecos) {
 int contaPalavras(char** enderecosComando, int numeroEnderecos) {
     int i = 0;
     int numeroPalavras = 0;
+    int flag = 0;
     while (i < numeroEnderecos && strcmp(enderecosComando[i], "&") != 0) {
-        numeroPalavras++;
         i++;
     }
+    numeroPalavras = i;
     return numeroPalavras;
 }
 
@@ -59,68 +56,41 @@ int main(int argc, char *argv[]) {
     char **path;
     int numeroEnderecos = 0;
     getcwd(diretorio, sizeof(diretorio));
-    printf("-- Bob shell --\n\n\n");
     char** enderecosComando;
     int numeroPalavras;
     int enderecoAtual = 0;
     int i;
+    int numeroProcessosPricipal = 0;
     int pathTamanho = 1;
-    
+    pid_t pidPrincipal = 1;
     pid_t pid;
     path = malloc(1 * sizeof(char*));
-path[0] = malloc(256 * sizeof(char));
+    path[0] = malloc(256 * sizeof(char));
     strcpy(path[0], "/usr/bin");
-    // Obtém o caminho absoluto do executável ls
     char ls_path[512];
     char cat_path[512];
     realpath("./ls", ls_path);
     realpath("./cat", cat_path);
 
+    printf("-- Bob shell --\n\n\n");
     while (1) {
         printf("\n%s>", diretorio);
         fgets(comando, 8191, stdin);
         comando[strcspn(comando, "\n")] = 0;
         enderecosComando = separarComando(comando, &numeroEnderecos);
-
-        numeroPalavras = contaPalavras(&enderecosComando[enderecoAtual], numeroEnderecos);
-
-        printf("\nEndereço atual: %d\n", enderecoAtual);
-        printf("\nNumero de palavras: %d\n", numeroPalavras);
-
-       // Verifica por redirecionamento de saída
-        int redirect_output = 0;
-        char *output_file = NULL;
-        for (i = 0; i < numeroEnderecos; i++) {
-            if (strcmp(enderecosComando[i], ">") == 0 && i + 1 < numeroEnderecos) {
-                redirect_output = 1;
-                output_file = enderecosComando[i + 1];
-                enderecosComando[i] = NULL; // Termina o comando antes do '>'
-                numeroEnderecos = i; // Ajusta o número de endereços para não incluir o redirecionamento
-                break;
+        for(i = 0;i<numeroEnderecos;i++){
+            if(strcmp(enderecosComando[i], "&") == 0 && pidPrincipal != 0){
+                numeroProcessosPricipal++;
+                pidPrincipal = fork();
+                if(pidPrincipal == 0){
+                    enderecoAtual = i + 1;
+                }
             }
         }
 
-        int stdout_backup;
-        if (redirect_output) {
-            stdout_backup = dup(fileno(stdout));
-            int output_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (output_fd == -1) {
-                perror("Erro ao abrir o arquivo de saída");
-                continue;
-            }
-            if (dup2(output_fd, fileno(stdout)) == -1) {
-                perror("Erro ao redirecionar stdout");
-                close(output_fd);
-                continue;
-            }
-            close(output_fd);
-        }
-
-        if (numeroEnderecos == 0) {
-            continue;  // No command to execute
-        }
-
-        if (strcmp(enderecosComando[0], "cd") == 0) {
+        numeroPalavras = contaPalavras(&enderecosComando[enderecoAtual], numeroEnderecos - enderecoAtual);
+    
+        if (strcmp(enderecosComando[enderecoAtual], "cd") == 0) {
             enderecoAtual++;
             if (chdir(enderecosComando[enderecoAtual]) == 0) {
                 getcwd(diretorio, sizeof(diretorio));
@@ -128,108 +98,103 @@ path[0] = malloc(256 * sizeof(char));
             else {
                 perror("chdir");
             }
-        } else if (strcmp(enderecosComando[0], "ls") == 0) {
-    pid = fork();
-    if (pid == 0) {
-        // Count the number of arguments to pass to execl
-        int arg_count = 1; // For "ls"
+        } else if (strcmp(enderecosComando[enderecoAtual], "ls") == 0) {
+            pid = fork();
+            if (pid == 0) {
+                char *args[numeroPalavras+1]; 
+                args[0] = enderecosComando[enderecoAtual];
 
-        // Create an array to hold the arguments
-        char *args[numeroPalavras]; // +1 for NULL termination
-
-        // Set the first argument to "ls"
-        args[0] = enderecosComando[0];
-
-        // Copy the rest of the arguments
-        for (int i = 1; i < numeroPalavras; i++) {
-            args[i] = enderecosComando[i];
-        }
-
-        // Null-terminate the arguments array
-        args[numeroPalavras] = NULL;
-
-       // Executa o comando com os argumentos
-        execv(ls_path, args);
-
-        // If execv fails
-        perror("execv");
-        exit(EXIT_FAILURE);
-    }
-    wait(NULL);
-}
-else if (strcmp(enderecosComando[enderecoAtual], "path") == 0) {
-            enderecoAtual++;
-            if (numeroPalavras == (enderecoAtual)) {
-                printf("PATH = ");
-                for (i = 0; i < pathTamanho; i++) {
-                    printf("%s ", path[i]);
+                for (int i = 1; i < numeroPalavras; i++) {
+                    args[i] = enderecosComando[enderecoAtual+i];
                 }
-                printf("\n");
-            } else {
-                for (i = 0; i < pathTamanho; i++) {
-                    free(path[i]);
-                } 
-                free(path);
-                pathTamanho = 0;
-                path = malloc((numeroPalavras - 1) * sizeof(char*));
-                for (i = 0; i < (numeroPalavras - 1); i++) {
-                    path[i] = malloc(256 * sizeof(char));
-                    strncpy(path[i],enderecosComando[enderecoAtual + i],255);
-                    
-                }
-                pathTamanho = i;
+                args[numeroPalavras] = NULL;
+                printf("\n%s",ls_path);
+                execv(ls_path, args);
+
+                perror("execv");
+                exit(EXIT_FAILURE);
             }
-        } else if(strcmp(enderecosComando[enderecoAtual],"cat") == 0){
-    enderecoAtual++;
-    pid_t pid = fork(); // cria um novo processo
-    if (pid == -1) {
-        perror("fork");
-    } else if (pid == 0) {
-        char *argv[] = {cat_path, enderecosComando[enderecoAtual], NULL}; // argumentos para o programa cat
-        execvp(argv[0], argv); // executa o programa cat no novo processo
-        perror("execvp"); // execvp retorna apenas se ocorrer um erro
-        exit(EXIT_FAILURE);
-    } else {
-        int status;
-        waitpid(pid, &status, 0); // espera o processo filho terminar
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-            fprintf(stderr, "O programa cat terminou com um erro.\n");
-        }
-    }
-}
- else if(strcmp(enderecosComando[0],"clear") == 0){
-            system("clear");
-
-        } else if(strcmp(enderecosComando[0],"exit") == 0){
-            printf("\nTerminou o BobShell\n");
-            free(path);
-            return 0;
-        }else{
-            int numeroProcessos = 0;
-            char diretorioProcurado[256];
-    
-            for(i = 0;i<pathTamanho;i++){
-                snprintf(diretorioProcurado, sizeof(diretorioProcurado), "%s/%s", path[i], enderecosComando[0]);
-                printf("%s\n",diretorioProcurado);
-                if(access(diretorioProcurado, X_OK) == 0){
-                    pid = fork();
-                    if(pid == 0){
-                        execl(diretorioProcurado, enderecosComando[0] , NULL, NULL);
+            wait(NULL);
+            }
+            else if (strcmp(enderecosComando[enderecoAtual], "path") == 0) {
+                enderecoAtual++;
+                if (numeroPalavras == 1) {
+                    printf("PATH = ");
+                    for (i = 0; i < pathTamanho; i++) {
+                        printf("%s ", path[i]);
                     }
-                    numeroProcessos++;
+                    printf("\n");
+                } else {
+                    for (i = 0; i < pathTamanho; i++) {
+                        free(path[i]);
+                    } 
+                    free(path);
+                    pathTamanho = 0;
+                    path = malloc((numeroPalavras - 1) * sizeof(char*));
+                    for (i = 0; i < (numeroPalavras - 1); i++) {
+                        path[i] = malloc(256 * sizeof(char));
+                        strncpy(path[i],enderecosComando[enderecoAtual + i],255);
+                        
+                    }
+                    pathTamanho = i;
+                }
+            } else if(strcmp(enderecosComando[enderecoAtual],"cat") == 0){
+                enderecoAtual++;
+                pid_t pid = fork(); // cria um novo processo
+                if (pid == -1) {
+                    perror("fork");
+                } else if (pid == 0) {
+                    char *argv[] = {cat_path, enderecosComando[enderecoAtual], NULL}; // argumentos para o programa cat
+                    execvp(argv[0], argv); // executa o programa cat no novo processo
+                    perror("execvp"); // execvp retorna apenas se ocorrer um erro
+                    exit(EXIT_FAILURE);
+                } else {
+                    int status;
+                    wait(NULL);
                 }
             }
-            
-            for(i = 0; i<numeroProcessos;i++)
-                wait(NULL);
-            if(numeroProcessos == 0){
-                printf("%s não é reconhecido como comando interno\n",enderecosComando[0]);
-            }
+            else if(strcmp(enderecosComando[0],"clear") == 0){
+                        system("clear");
+
+            } else if(strcmp(enderecosComando[0],"exit") == 0){
+                printf("\nTerminou o BobShell\n");
+                free(path);
+                return 0;
+            }else{
+                int numeroProcessos = 0;
+                char diretorioProcurado[256];
+                
+                for(i = 0;i<pathTamanho;i++){
+                    snprintf(diretorioProcurado, sizeof(diretorioProcurado), "%s/%s", path[i], enderecosComando[enderecoAtual]);
+                    if(access(diretorioProcurado, X_OK) == 0){
+                        pid = fork();
+                        if(pid == 0){
+                            execl(diretorioProcurado, enderecosComando[enderecoAtual] , NULL, NULL);
+                        }
+                        numeroProcessos++;
+                    }
+                }
+                    
+                for(i = 0; i<numeroProcessos;i++)
+                    wait(NULL);
+                if(numeroProcessos == 0){
+                    printf("%s não é reconhecido como comando interno\n",enderecosComando[enderecoAtual]);
+                }
         }
 
         free(enderecosComando);
+        
+        
         numeroPalavras = 0;
         enderecoAtual = 0;
+
+        if(pidPrincipal == 0){
+            return 0;
+        }
+        for(i = 0; i<numeroProcessosPricipal;i++)
+            wait(NULL);
+
+        numeroProcessosPricipal = 0;
     }
 
    
